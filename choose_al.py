65863ddcd7f8c8cdc338fa79938f2c4afe850bc1
unknown <dustin36@naver.com>
2019-10-07 15:49:47 +0900

    first commit

--- choose_al.py
+++ choose_al.py
@@ -1,23 +1,46 @@
 def sort(List):
     
     num = 1
-    max = 0
+    max = List[0]
     idx = 0
 
     for j in range(len(List)):
         for i in range(len(List) - num + 1):
             temp = List[i]
-            if max < temp:
+            if max <= temp:
                 max = temp
                 idx = i
         num2 = List[len(List) - num]
         List[len(List) - num] = max
         List[idx] = num2
         num += 1
-        max = 0
+        max = List[0]
+
+def sort2(List):
+    
+    num = 1
+    min = List[0]
+    idx = 0
+
+    for j in range(len(List)):
+        for i in range(len(List) - num + 1):
+            temp = List[i]
+            if min >= temp:
+                min = temp
+                idx = i
+        num2 = List[len(List) - num]
+        List[len(List) - num] = min
+        List[idx] = num2
+        num += 1
+        min = List[0]
+    List.reverse()
+
 
 List = [6,2,1,4,5,3]
+List2 = [6,2,1,4,5,3]
 
 sort(List)
+sort2(List2)
 
 print(List)
+print(List2)
--- insert_al.py
+++ insert_al.py
@@ -1,19 +1,22 @@
 def sort(List):
-    
     for i in range(len(List) - 1):
-        num = 0
-        while(i - num >= 0):
-            print(List[i + 1],List[i - num])
-            print(List)
-            if List[i + 1] < List[i - num]:
-                temp = List[i + 1]
-                List[i + 1] = List[i - num]
-                List[i - num] = temp
-                i -= 1
-            num += 1
+        num1 = 0
+        num2 = 1
+        while(True):
+            if (List[i + 1 - num1] < List[i + 1 - num2]):
+                temp = List[i + 1 - num1]
+                List[i + 1 - num1] = List[i + 1 - num2]
+                List[i + 1 - num2] = temp
+                if (i + 1 - num2 > 0):
+                    num1 += 1
+                    num2 += 1
+                else:
+                    break
+            else:
+                break
 
 List = [6,2,1,4,5,3]
 
 sort(List)
 
-#print(List)
+print(List)
